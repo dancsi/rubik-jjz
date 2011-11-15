4e7ffbdce5ab71ebb8fd52c68f86bfc297581d45
Ostoja <momomje@yahoo.com>
2011-11-15 15:12:14 +0100

    Update rubik.pas

--- rubik.pas
+++ rubik.pas
@@ -3,7 +3,7 @@ Program rubik;
 Uses algorithm, output;
 
 Begin
-  writeln('Welcome to the most best Rubik solving program koji je ovaj grad ikada video'); {Bestest!? Now it's grammaticly correct lol}
+  writeln('Welcome to the best Rubik solving program koji je ovaj grad ikada video'); {Bestest!? Now it's grammaticly correct lol}
   SayHelloA();
   SayHelloO();
 End.
