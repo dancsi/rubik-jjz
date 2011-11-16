98a5ef7a3489baa95c2ad56d7817d28ca25ff7f9
dancsi <szilagyi.d@gmail.com>
2011-11-16 07:41:23 +0100

    Added function prototypes

--- cube.pp
+++ cube.pp
@@ -1,10 +1,25 @@
-
 Unit algorithm;
 
 Interface
 
 Type Face =   array[1..3,1..3] Of char;
+Type Cube = record
+              F, B, U, D, L, R:Face;
+            end;
+
 Procedure sayHelloA;
+Procedure TurnU(var c: Cube);
+Procedure TurnIU(var c: Cube);
+Procedure TurnD(var c: Cube);
+Procedure TurnID(var c: Cube);
+Procedure TurnF(var c: Cube);
+procedure TurnIF(var c: Cube);
+procedure TurnB(var c: Cube);
+procedure TurnIB(var c: Cube);
+procedure TurnR(var c: Cube);
+procedure TurnIR(var c: Cube);
+procedure TurnL(var c: Cube);
+procedure TurnIL(var c: Cube);
 
 Implementation
 
