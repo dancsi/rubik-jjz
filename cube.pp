e24e09dc65d69d24bf4b582122fbf2f51b6f387c
dancsi <szilagyi.d@gmail.com>
2011-11-16 08:17:30 +0100

    started matrix rotation code

--- cube.pp
+++ cube.pp
@@ -7,7 +7,6 @@ Type Cube = record
               F, B, U, D, L, R:Face;
             end;
 
-Procedure sayHelloA;
 Procedure TurnU(var c: Cube);
 Procedure TurnIU(var c: Cube);
 Procedure TurnD(var c: Cube);
@@ -21,11 +20,55 @@ procedure TurnIR(var c: Cube);
 procedure TurnL(var c: Cube);
 procedure TurnIL(var c: Cube);
 
+procedure TurnFaceCW(var f: Face);
+procedure TurnFaceCCW(var f: Face);
+
+Procedure Swap(var a, b:char);
+
 Implementation
 
-Procedure sayHelloA;
-Begin
-writeln('Hello from algorithm');
-End;
+Procedure Swap(var a, b:char);
+var t:char;
+begin
+	t:=a;
+	a:=b;
+	b:=t;
+end;
+
+procedure TurnFaceCW(var f: Face);
+var		i, j:integer;
+			t: Face;
+begin
+	for i:=1 to 3 do begin
+		for j:=1 to 3 do begin
+			t[3, 1]:=f[1, 1];
+			t[2, 3]:=f[][]
+		end;
+	end;
+end;
+
+procedure TurnFaceCCW(var f: Face);
+var tmp: Face;
+		i, j:integer;
+begin
+
+end;
+
+Procedure TurnU(var c: Cube);
+var i, j:integer;
+begin
+	
+end;
+Procedure TurnIU(var c: Cube);
+Procedure TurnD(var c: Cube);
+Procedure TurnID(var c: Cube);
+Procedure TurnF(var c: Cube);
+procedure TurnIF(var c: Cube);
+procedure TurnB(var c: Cube);
+procedure TurnIB(var c: Cube);
+procedure TurnR(var c: Cube);
+procedure TurnIR(var c: Cube);
+procedure TurnL(var c: Cube);
+procedure TurnIL(var c: Cube);
 
 End.
