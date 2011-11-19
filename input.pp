4456f5f28676cd772200d660b29766352159ddba
Daniel Siladji <daniel.siladji@gmail.com>
2011-11-19 10:00:47 +0100

    More reorganization

--- input.pp
+++ input.pp
@@ -3,9 +3,11 @@ Unit input;
 
 Interface
 
-Uses RubiksCube, algorithm;
+Uses RubiksCube, algorithm, glut;
 
 var buff: string;
+	prevX, prevY:longint;
+	TrackingMouseMovement: boolean;
 procedure ProcessKeyboard(Key: Byte; X, Y: Longint; var c: Cube);
 procedure ProcessMouse(button, state, x ,y:longint; var c: Cube);
 procedure ProcessMouseMotion(x, y:Longint; var c: Cube);
@@ -26,13 +28,30 @@ Begin
     Else If ch='I' Then buff := 'I';
     writeln(ch);
 End;
+
+procedure StartUpdatingRotation(x, y:longint);
+begin
+	TrackingMouseMovement:=true;
+	prevX:=x;
+	prevY:=y;
+end;
+
+procedure StopUpdatingRotation();
+begin
+	TrackingMouseMovement:=false;
+end;
+
 procedure ProcessMouse(button, state, x ,y:longint; var c: Cube);
 begin
-
+	if (button=GLUT_LEFT_BUTTON) and (state=GLUT_DOWN) then begin
+		StartUpdatingRotation(x, y);
+	end else if (button=GLUT_LEFT_BUTTON) and (state=GLUT_DOWN) then begin
+		StopUpdatingRotation();
+	end;
 end;
 procedure ProcessMouseMotion(x, y:Longint; var c: Cube);
 begin
-	
+	if 
 end;
 
 End.
